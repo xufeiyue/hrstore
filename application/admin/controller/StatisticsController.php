8c6de19df4f7032ecbf75f73f80a713c7a051a20
xufeiyue <369389714@qq.com>
2019-03-21 11:42:45 +0800

    1

--- application/admin/controller/StatisticsController.php
+++ application/admin/controller/StatisticsController.php
@@ -24,6 +24,7 @@ class StatisticsController extends AdminController{
     }
    // 统计结果
     public function statistics_list(){
+        // 默认全平台各种优惠券的领取数量
         
         return json(["code" =>  0, "msg" => "请求成功", 'data' => $data , 'count' => 2]);
     }
